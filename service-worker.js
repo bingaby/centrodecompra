bc99188e8c7ad4cca7887f752d9e7b74845a8ced
Carlos Augusto vieira <91630125+bingaby@users.noreply.github.com>
2025-06-07 20:47:59 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,24 @@
+const CACHE_NAME = 'centro-de-compras-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
+
 self.addEventListener('install', event => {
   console.log('Service Worker instalado');
 });
