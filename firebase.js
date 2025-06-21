667ed595f856627361d77c964f9475957dd41382
Carlos Augusto vieira <91630125+bingaby@users.noreply.github.com>
2025-06-21 17:21:07 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,36 +1,22 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
-import {
-  getFirestore,
-  collection,
-  query,
-  orderBy,
-  limit,
-  startAfter,
-  getDocs
-} from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
-// Sua config do Firebase (preencha com seus dados)
 const firebaseConfig = {
   apiKey: "AIzaSyDryJc0Y7JV_Os5DZdEDts5XaFUEtJ7wDk",
   authDomain: "centrodecompra-5fa91.firebaseapp.com",
   projectId: "centrodecompra-5fa91",
-  storageBucket: "centrodecompra-5fa91.appspot.com", // corrigido .app -> .appspot.com
+  storageBucket: "centrodecompra-5fa91.firebasestorage.app",
   messagingSenderId: "276696026262",
   appId: "1:276696026262:web:979a68c0796ea1d17346b7",
   measurementId: "G-PM11NQL61N"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-export {
-  db,
-  collection,
-  query,
-  orderBy,
-  limit,
-  startAfter,
-  getDocs
-};
+export { app, db, storage, analytics };
