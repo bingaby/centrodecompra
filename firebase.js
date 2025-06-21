55ffdca99d96f059a1b3eea59d7c5dc6ef520e7a
Carlos Augusto vieira <91630125+bingaby@users.noreply.github.com>
2025-06-21 10:12:44 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDryJc0Y7JV_Os5DZdEDts5XaFUEtJ7wDk",
+  authDomain: "centrodecompra-5fa91.firebaseapp.com",
+  projectId: "centrodecompra-5fa91",
+  storageBucket: "centrodecompra-5fa91.appspot.com",
+  messagingSenderId: "276696026262",
+  appId: "1:276696026262:web:979a68c0796ea1d17346b7",
+  measurementId: "G-PM11NQL61N"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+// Exporta para uso global
+window.firebaseDb = db;
+window.firebaseStorage = storage;
+window.firebaseAnalytics = analytics;
