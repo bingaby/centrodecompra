8172ef945f565db502b3ef6bbc2b28c20d17e101
Carlos Augusto vieira <91630125+bingaby@users.noreply.github.com>
2025-06-23 10:48:51 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,16 @@
-// firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDryJc0Y7JV_Os5DZdEDts5XaFUEtJ7wDk",
-  authDomain: "centrodecompra-5fa91.firebaseapp.com",
-  projectId: "centrodecompra-5fa91",
-  storageBucket: "centrodecompra-5fa91.firebasestorage.app",
-  messagingSenderId: "276696026262",
-  appId: "1:276696026262:web:979a68c0796ea1d17346b7",
-  measurementId: "G-PM11NQL61N"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
-export { app, db, storage, analytics, auth };
+{
+  "hosting": {
+    "public": "dist",
+    "rewrites": [
+      {
+        "source": "/favicon.ico",
+        "destination": "/logos/favicon-32x32.png"
+      }
+    ],
+    "ignore": [
+      "firebase.json",
+      "**/.*",
+      "**/node_modules/**"
+    ]
+  }
+}
