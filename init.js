1496f87b6a65b4b114ef0597391fe5939e3c445d
Carlos Augusto vieira <91630125+bingaby@users.noreply.github.com>
2025-08-26 12:50:43 -0300

    Update init.js

--- init.js
+++ init.js
@@ -5,6 +5,7 @@
   // Inicializar Google Ads
   if (typeof adsbygoogle !== 'undefined') {
     (adsbygoogle = window.adsbygoogle || []).push({});
+    console.log('Google Ads inicializado');
   } else {
     console.warn('Google Ads não inicializado. Verifique a inclusão do script adsbygoogle.js.');
   }
